ff5d120d5a6b7c237e458e8fc28815df3217a503
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-12-04 14:45:42 -0200

    Campos tela funcionarios

--- UninoveAcademiaSistemas/Formularios/Cadastro/frmFuncionario.cs
+++ UninoveAcademiaSistemas/Formularios/Cadastro/frmFuncionario.cs
@@ -16,5 +16,10 @@ namespace UninoveAcademiaSistemas.Formularios.Cadastro
         {
             InitializeComponent();
         }
+
+        private void tsbFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
