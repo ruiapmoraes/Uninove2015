ea7e3163a016fe91ee51a5cf215eec735a600188
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-12-05 00:43:33 -0200

    Tela de Horario

--- UninoveAcademiaSistemas/Formularios/Exercicios/frmHorario.Designer.cs
+++ UninoveAcademiaSistemas/Formularios/Exercicios/frmHorario.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace UninoveAcademiaSistemas.Formularios.Cadastro
+﻿namespace UninoveAcademiaSistemas.Formularios.Exercicios
 {
-    partial class frmAula
+    partial class frmHorario
     {
         /// <summary>
         /// Required designer variable.
@@ -29,50 +29,40 @@
         private void InitializeComponent()
         {
             this.gbAula = new System.Windows.Forms.GroupBox();
-            this.txtCargo = new System.Windows.Forms.TextBox();
-            this.txtCodigo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.gbAula.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbAula
             // 
-            this.gbAula.Controls.Add(this.txtCargo);
-            this.gbAula.Controls.Add(this.txtCodigo);
+            this.gbAula.Controls.Add(this.maskedTextBox1);
+            this.gbAula.Controls.Add(this.dateTimePicker2);
+            this.gbAula.Controls.Add(this.dateTimePicker1);
+            this.gbAula.Controls.Add(this.label3);
             this.gbAula.Controls.Add(this.label2);
             this.gbAula.Controls.Add(this.label1);
-            this.gbAula.Location = new System.Drawing.Point(23, 12);
+            this.gbAula.Location = new System.Drawing.Point(12, 12);
             this.gbAula.Name = "gbAula";
-            this.gbAula.Size = new System.Drawing.Size(266, 129);
-            this.gbAula.TabIndex = 6;
+            this.gbAula.Size = new System.Drawing.Size(221, 151);
+            this.gbAula.TabIndex = 9;
             this.gbAula.TabStop = false;
-            this.gbAula.Text = "Aula";
-            // 
-            // txtCargo
-            // 
-            this.txtCargo.Location = new System.Drawing.Point(110, 78);
-            this.txtCargo.Name = "txtCargo";
-            this.txtCargo.Size = new System.Drawing.Size(140, 20);
-            this.txtCargo.TabIndex = 3;
-            // 
-            // txtCodigo
-            // 
-            this.txtCodigo.Location = new System.Drawing.Point(110, 41);
-            this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(140, 20);
-            this.txtCodigo.TabIndex = 2;
+            this.gbAula.Text = "Horário";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(40, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Aula:";
+            this.label2.Text = "Data:";
             // 
             // label1
             // 
@@ -85,32 +75,67 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(133, 147);
+            this.btnSalvar.Location = new System.Drawing.Point(77, 178);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(75, 23);
-            this.btnSalvar.TabIndex = 7;
+            this.btnSalvar.TabIndex = 10;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(214, 147);
+            this.btnCancelar.Location = new System.Drawing.Point(158, 178);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
-            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.TabIndex = 11;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
             // 
-            // frmAula
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hora:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(110, 75);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(89, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(110, 106);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(89, 20);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(110, 36);
+            this.maskedTextBox1.Mask = "00000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(89, 20);
+            this.maskedTextBox1.TabIndex = 8;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // frmHorario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 193);
+            this.ClientSize = new System.Drawing.Size(253, 212);
             this.Controls.Add(this.gbAula);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.btnCancelar);
-            this.Name = "frmAula";
-            this.Text = "Cadastro de Aula";
+            this.Name = "frmHorario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Horário";
             this.gbAula.ResumeLayout(false);
             this.gbAula.PerformLayout();
             this.ResumeLayout(false);
@@ -120,11 +145,13 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gbAula;
-        private System.Windows.Forms.TextBox txtCargo;
-        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
